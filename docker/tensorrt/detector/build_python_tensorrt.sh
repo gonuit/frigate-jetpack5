baadd5b7ed78ad1039e6ba51569fabe9f84d81f8
Kamil Klyta <37882650+gonuit@users.noreply.github.com>
2026-07-18 03:10:44 +0200

    Fix arm64 TensorRT image build on JetPack 5 (TensorRT 8.5 / CUDA 11.4)

--- docker/tensorrt/detector/build_python_tensorrt.sh
+++ docker/tensorrt/detector/build_python_tensorrt.sh
@@ -12,17 +12,18 @@ if [[ "${TARGETARCH}" == "arm64" ]]; then
   # Get python-tensorrt source
   mkdir -p /workspace
   cd /workspace
-  git clone -b release/8.6 https://github.com/NVIDIA/TensorRT.git --depth=1
+  git clone -b "release/${TENSORRT_VER%.*}" https://github.com/NVIDIA/TensorRT.git --depth=1
 
   # Collect dependencies
   EXT_PATH=/workspace/external && mkdir -p $EXT_PATH
-  pip3 install pybind11 && ln -s /usr/local/lib/python3.11/dist-packages/pybind11 $EXT_PATH/pybind11
-  ln -s /usr/include/python3.11 $EXT_PATH/python3.11
+  pip3 install pybind11 && ln -s "$(pip3 show pybind11 | grep -Po '^Location: \K.*')/pybind11" $EXT_PATH/pybind11
+  ln -s "$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["include"])')" $EXT_PATH/python3.11
   ln -s /usr/include/aarch64-linux-gnu/NvOnnxParser.h /workspace/TensorRT/parsers/onnx/
 
   # Build wheel
   cd /workspace/TensorRT/python
   EXT_PATH=$EXT_PATH PYTHON_MAJOR_VERSION=3 PYTHON_MINOR_VERSION=11 TARGET_ARCHITECTURE=aarch64 TENSORRT_MODULE=tensorrt /bin/bash ./build.sh
-  mv build/bindings_wheel/dist/*.whl /trt-wheels/
+  find build -name 'tensorrt-*.whl' -exec mv -v {} /trt-wheels/ \;
+  ls /trt-wheels/tensorrt-*.whl
 
 fi
